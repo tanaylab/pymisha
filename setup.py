f078a17138f5e1be0735de564b4e871e273aa66a
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2026-02-13 15:29:31 +0200

    Add CI/CD workflows and PyPI publishing setup
    - ci.yml: test matrix (Python 3.10-3.12 x Linux/macOS) on push/PR
    - publish.yml: cibuildwheel + trusted publishing on tag push
    - MANIFEST.in: include C++ sources in sdist
    - setup.py: clean up duplicate metadata, add -O2

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
 import glob
+import os
 
 import numpy
 from setuptools import Extension, setup
 
-# Get source files
-src_files = glob.glob('src/*.cpp')
+# Get source files relative to setup.py location
+here = os.path.abspath(os.path.dirname(__file__))
+src_files = sorted(glob.glob(os.path.join(here, 'src', '*.cpp')))
 
 setup(
-    name='pymisha',
-    version='0.1.0',
-    description='Python wrapper for misha - Genomic Data Analysis Toolkit',
-    author='Aviezer Lifshitz',
-    author_email='aviezerl@weizmann.ac.il',
-    url='https://github.com/tanaylab/pymisha',
-    install_requires=['pandas', 'numpy'],
-    py_modules=['pymisha'],
-    packages=['pymisha'],
     ext_modules=[
         Extension('_pymisha',
             sources=src_files,
-            include_dirs=[numpy.get_include(), 'src'],
+            include_dirs=[numpy.get_include(), os.path.join(here, 'src')],
             extra_compile_args=[
                 '-std=c++17',
+                '-O2',
                 '-Wno-unused-function',
                 '-Wno-switch',
                 '-Wno-strict-aliasing',
@@ -30,6 +24,5 @@ setup(
             ],
         ),
     ],
-    python_requires='>=3.10',
-    zip_safe=False
+    zip_safe=False,
 )
